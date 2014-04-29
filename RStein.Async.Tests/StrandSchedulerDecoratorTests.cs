a97c1c59e6972a9a82ad5db54572ab5346590ee8
Rene Stein <rene@renestein.net>
2014-04-29 16:38:15 +0200

    Added reference to TimePeriodLibrary

--- RStein.Async.Tests/StrandSchedulerDecoratorTests.cs
+++ RStein.Async.Tests/StrandSchedulerDecoratorTests.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RStein.Async.Misc;
 using RStein.Async.Schedulers;
 
 namespace RStein.Async.Tests
@@ -7,7 +13,7 @@ namespace RStein.Async.Tests
   [TestClass]
   public class StrandSchedulerDecoratorTests : IAutonomousSchedulerTests
   {
-    public const int NUMBER_OF_THREAD = 4;
+    public const int NUMBER_OF_THREADS = 4;
     private ITaskScheduler m_strandScheduler;
     private ExternalProxyScheduler m_externalScheduler;
     private ITaskScheduler m_innerScheduler;
@@ -16,7 +22,7 @@ namespace RStein.Async.Tests
     {
       m_innerScheduler = CreateInnerScheduler();
       m_strandScheduler = new StrandSchedulerDecorator(m_innerScheduler);
-      m_externalScheduler = new ExternalProxyScheduler(m_strandScheduler);      
+      m_externalScheduler = new ExternalProxyScheduler(m_strandScheduler);
       base.InitializeTest();
     }
 
@@ -30,7 +36,7 @@ namespace RStein.Async.Tests
     protected virtual ITaskScheduler CreateInnerScheduler()
     {
       var ioService = new IoServiceScheduler();
-      return new IoServiceThreadPoolScheduler(ioService, NUMBER_OF_THREAD);
+      return new IoServiceThreadPoolScheduler(ioService, NUMBER_OF_THREADS);
     }
 
     protected override ITaskScheduler Scheduler
@@ -47,6 +53,46 @@ namespace RStein.Async.Tests
       {
         return m_externalScheduler;
       }
+    }
+
+    [TestMethod]
+    public async Task WithTaskFactory_When_Tasks_Added_All_Tasks_Executed_Sequentially()
+    {
+      const int numberOfTasks = 10;
+      const int DEFAULT_THREAD_SLEEP = 200;
+      var tasks = Enumerable.Range(0, numberOfTasks)
+                .Select(i => CurrentTaskFactory.StartNew(() =>
+                                                         {
+                                                           Thread.Sleep(DEFAULT_THREAD_SLEEP);
+                                                           return DateTime.Now;
+                                                         }))
+                .ToArray();
+
+      await Task.WhenAll(tasks);
+
+      var allTasksExecutedSequentially = tasks.Aggregate(
+                                              new
+                                              {
+                                                Result = true,
+                                                PreviousTask = (Task<DateTime>)null
+                                              },
+                                              (prevResult, currentTask) =>
+                                              {
+                                                if (!prevResult.Result)
+                                                {
+                                                  return prevResult;
+                                                }
+
+                                                return new
+                                                       {
+                                                         Result = (prevResult.PreviousTask != null
+                                                           ? currentTask.Result > prevResult.PreviousTask.Result : prevResult.Result),
+                                                         PreviousTask = currentTask
+                                                       };
+                                              },
+                                              resultPair => resultPair.Result);
+
+      Assert.IsTrue(allTasksExecutedSequentially);
     }
   }
 }
