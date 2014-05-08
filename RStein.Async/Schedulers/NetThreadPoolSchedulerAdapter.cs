87a88ca5757fd1055ee7e4580033a6c084f76ced
Rene Stein <rene@renestein.net>
2014-05-08 08:54:50 +0200

    Added NetThreadPoolSchedulerAdapter

---
+++ RStein.Async/Schedulers/NetThreadPoolSchedulerAdapter.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RStein.Async.Schedulers
+{
+  public class NetThreadPoolSchedulerAdapter : TaskSchedulerBase
+  {
+    public override void QueueTask(Task task)
+    {
+      checkIfDisposed();
+      createThreadPoolTask(task);
+    }
+
+    public override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+    {
+      checkIfDisposed();
+      if (taskWasPreviouslyQueued)
+      {
+        return false;
+      }
+
+      createThreadPoolTask(task);
+
+      //TODO: Truth or lie?
+      return true;
+    }
+
+    public override IEnumerable<Task> GetScheduledTasks()
+    {
+      checkIfDisposed();
+      //TODO: Truth or lie?
+      return Enumerable.Empty<Task>();
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+      
+    }
+
+    private void createThreadPoolTask(Task originalTask)
+    {
+      var task = new Task(originalTask.RunSynchronously);
+      task.Start(TaskScheduler.Default);
+    }
+
+    public override int MaximumConcurrencyLevel
+    {
+      get
+      {
+        return TaskScheduler.Default.MaximumConcurrencyLevel;
+      }
+    }
+  }
+}
