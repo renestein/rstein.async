ce9a688e8348413e44a59531cbd9441bd2a2aa29
Rene Stein <rene@renestein.net>
2014-05-27 12:50:37 +0200

    Added DebugTaskCompletionSource

---
+++ RStein.Async/Tasks/BrokenPromiseException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RStein.Async.Tasks
+{
+  [Serializable]
+  public class BrokenPromiseException : Exception
+  {
+    public BrokenPromiseException()
+    {
+    }
+    public BrokenPromiseException(string message)
+      : base(message)
+    {
+    }
+    public BrokenPromiseException(string message, Exception inner)
+      : base(message, inner)
+    {
+    }
+
+    protected BrokenPromiseException(
+      SerializationInfo info,
+      StreamingContext context)
+      : base(info, context)
+    {
+    }
+  }
+}
---
+++ RStein.Async/Tasks/DebugTaskCompletionSource.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace RStein.Async.Tasks
+{
+  public class DebugTaskCompletionSource<T>
+  {
+    public const string DEFAULT_BROKEN_PROMISE_DESCRIPTION = "Broken promise DebugTaskCompletionSource - task id {0}";
+    private static ConcurrentDictionary<Object, Task> _tasksDictionary = new ConcurrentDictionary<object, Task>();
+
+    private TaskCompletionSource<T> m_taskCompletionSource;
+    private string m_description;
+    private Task<T> m_task;
+
+    public DebugTaskCompletionSource(string description)
+    {
+
+      m_taskCompletionSource = new TaskCompletionSource<T>();
+      m_task = m_taskCompletionSource.Task;
+      m_description = description ?? String.Format(DEFAULT_BROKEN_PROMISE_DESCRIPTION, m_taskCompletionSource.Task.Id);
+      registerTask();
+    }
+
+#if DEBUG
+    ~DebugTaskCompletionSource()
+    {
+      detectBrokenPromise();
+
+    }
+#endif
+
+    [ConditionalAttribute("DEBUG")]
+    public static void DetectBrokenTaskCompletionSources()
+    {
+      GC.Collect();
+      GC.WaitForPendingFinalizers();
+      GC.Collect();
+    }
+
+    [ConditionalAttribute("DEBUG")]
+    private void registerTask()
+    {
+      Debug.Assert(_tasksDictionary.TryAdd(this, m_task));
+    }
+
+    [ConditionalAttribute("DEBUG")]
+    private void detectBrokenPromise()
+    {
+      Task task;
+      _tasksDictionary.TryGetValue(this, out task);
+
+      Debug.Assert(task != null);
+      if (!task.IsCompleted)
+      {
+        throw new BrokenPromiseException(m_description);
+      }
+    }
+  }
+}
